e1d864fb242a76a628c210b7d6973fa9f75113da
Anton Davidouski <antdav2005@gmail.com>
2022-11-02 21:00:13 +0000

    Ring placing and place preview kinda working

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,53 @@
 function makeBoard(){
+    //Creates a board div, places SVG inside and adds to main container, binds event listeners
     var mainContainer = document.getElementById("mainContainer");
     var board = document.createElement("img");
     board.src = "img/boardVector.svg";
     board.id = "board";
     board.className = "board";
-    board.addEventListener("click", mousePosition);
+    board.addEventListener("click", placeRing);
+    board.addEventListener("mousemove", hoverRing);
     mainContainer.appendChild(board);
 }
 
-function mousePosition(){
+function placeRing(){
+    //Places a ring on the board by takin the x and y coordinates of the click event
     var board = document.getElementById("board");
     var offsetX = board.offsetLeft;
     var offsetY = board.offsetTop;
     var x = event.pageX - offsetX;
-    var y = event.pageY - offsetY;; 
+    var y = event.pageY - offsetY;
     console.log(x + " " + y);
-    var marker = document.createElement("div");
-    marker.style.position = "absolute";
-    marker.style.left = (x + offsetX)+ "px";
-    marker.style.top = (y + offsetY) + "px";
-    marker.style.width = "10px";
-    marker.style.height = "10px";
-    marker.style.backgroundColor = "red";
-    mainContainer.appendChild(marker);
+    var ring = document.createElement("div");
+    ring.id = "permanent"
+    ring.className = "ring";
+    ring.style.position = "absolute";
+    ring.style.left = (x + offsetX)+ "px";
+    ring.style.top = (y + offsetY) + "px";
+    mainContainer.appendChild(ring);
 }
 
-
+function hoverRing(){
+    //Places a temporary ring on the board by taking the current x and y coordinates of the mouse while it is moving inside the board div
+    var oldRing = document.getElementById("temporary");
+    if(oldRing != null){
+        oldRing.remove();
+    }
+    var board = document.getElementById("board");
+    var offsetX = board.offsetLeft;
+    var offsetY = board.offsetTop;
+    var x = event.pageX - offsetX;
+    var y = event.pageY - offsetY;
+    var ring = document.createElement("div");
+    ring.id = "temporary"
+    ring.className = "ring";
+    ring.addEventListener("click", placeRing);
+    //Must bind placeRing listener to the temporary ring so you can still place a ring when hovering over the temporary one.
+    ring.style.position = "absolute";
+    ring.style.left = (x + offsetX)+ "px";
+    ring.style.top = (y + offsetY) + "px";
+    mainContainer.appendChild(ring);
+}
 
 window.onload = makeBoard;
 
